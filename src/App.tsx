7026e02e7da0fa14e1f2a33823a159dffc39db0e
matias <matias.tonello@gm2dev.com>
2023-03-24 01:49:05 -0300

    feat: fetch of images

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}/?json=true`;
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
 function App() {
-  const [fact, setFact] = useState("");
-  const [urlImage, setUrlImage] = useState();
+  const [fact, setFact] = useState<string>();
+  const [imageUrl, setImageUrl] = useState<string>();
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const threeFirstWords = fact.split(" ", 3).join(" ");
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?json=true`)
+          .then((response) => response.json())
+          .then((response) => {
+            const { url } = response;
+            setImageUrl(url);
+          });
+      });
   }, []);
 
   return (
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt={"image extracted using first word"}
+          height={500}
+        />
+      )}
     </main>
   );
 }
