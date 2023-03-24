06ef8bcd85e8257cde2a4e14aba75a876a67b8c2
matias <matias.tonello@gm2dev.com>
2023-03-24 00:59:46 -0300

    feat: adding endpoint for image url

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}/?json=true`;
+
 function App() {
   const [fact, setFact] = useState("");
+  const [urlImage, setUrlImage] = useState(null);
 
   const getFact = async () => {
     const fact = await fetch("https://catfact.ninja/fact").then((response) =>
@@ -11,8 +14,17 @@ function App() {
     setFact(fact.fact);
   };
 
+  const getImage = async () => {
+    const image = await fetch("https://cataas.com/cat/says/hola").then(
+      (response) => response.body
+    );
+
+    console.log(image);
+  };
+
   useEffect(() => {
     getFact();
+    getImage();
   }, []);
 
   return <div>{fact}</div>;
