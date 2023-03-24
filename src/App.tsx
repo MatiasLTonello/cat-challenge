5c9177df8ba3c49ec0b505b39176dcc76dddaed5
matias <matias.tonello@gm2dev.com>
2023-03-24 01:05:28 -0300

    fix: getting fact into use effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}/?json=true`;
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}/?json=true`;
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 
 function App() {
   const [fact, setFact] = useState("");
-  const [urlImage, setUrlImage] = useState(null);
-
-  const getFact = async () => {
-    const fact = await fetch("https://catfact.ninja/fact").then((response) =>
-      response.json()
-    );
-    setFact(fact.fact);
-  };
-
-  const getImage = async () => {
-    const image = await fetch("https://cataas.com/cat/says/hola").then(
-      (response) => response.body
-    );
-
-    console.log(image);
-  };
+  const [urlImage, setUrlImage] = useState();
 
   useEffect(() => {
-    getFact();
-    getImage();
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => setFact(data.fact));
   }, []);
 
-  return <div>{fact}</div>;
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+    </main>
+  );
 }
 
 export default App;
